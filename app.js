eb83e6c9b95213f6690108e7a9b8bec506b32fb6
JoseLucasapp <josluc2016@gmail.com>
2021-02-02 14:46:35 -0300

    update

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const ejs = require('ejs')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static('./public/'));
-app.set('view engine', ejs);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', ejs);
+app.use(express.static('./public/'));
 
-consign({cwd: process.cwd()+"/app"})
-    .include('/controllers')
-    .then('/routes')
-    .then('/models')
+consign({cwd: process.cwd()})
+    .include('app/controllers')
+    .then('app/routes')
+    .then('app/models')
     .into(app);
 
 app.listen(PORT);
