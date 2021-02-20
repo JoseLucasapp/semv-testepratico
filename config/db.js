9652bf3b5d03b8361f5511d41fc0b11c57762ae6
José Lucas <58341307+JoseLucasapp@users.noreply.github.com>
2021-02-20 01:36:19 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = {
-        mongoURI : 'mongodb+srv://jlucasgf:jjXIJxSgy9llN5fb@cluster0.hovld.mongodb.net/semvteste?retryWrites=true&w=majority'
+        mongoURI : 'mongodb://localhost/formulario'
 
     }
 }else{
     module.exports = {
-        mongoURI: "mongodb+srv://jlucasgf:jjXIJxSgy9llN5fb@cluster0.hovld.mongodb.net/semvteste?retryWrites=true&w=majority"
+        mongoURI: "mongodb://localhost/formulario"
     }
 }
